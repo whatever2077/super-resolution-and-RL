3f35abd71926c86b7c2e7b47e39853440189916f
超级厉害的萱萱 <luoxuan@MacBook-Pro-2.local>
2026-04-13 11:41:11 +0800

    first commit

--- config.py
+++ config.py
@@ -5,19 +5,17 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = ''
-TOKEN = ''
+USERNAME = 'whatever2077'
+TOKEN = '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'
 
 # The repository to add this issue to
-REPO_OWNER = ''
-REPO_NAME = ''
+REPO_OWNER = 'whatever2077'
+REPO_NAME = 'super-resolution-and-RL'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["", ]
+KEYWORD_LIST = ["image super-resolution", "super-resolution", "SR", "super resolution", "super-resolution reconstruction", "super-resolution restoration", "super-resolution enhancement", "super-resolution generation", "super-resolution prediction", "super-resolution synthesis", "super-resolution transformation", "super-resolution upscaling", "super-resolution upsampling","reinforcement learning", "RL", "deep reinforcement learning", "DRL", "reinforcement learning-based"]
 
-
-OPENAI_API_KEYS = ['', ]
-LANGUAGE = "zh"  # zh | en
+OPENAI_API_KEYS = ["sk-nG2b2a92a6ad1c499d24d5c93436a6d5a61ce779e1885ZQ9"]
