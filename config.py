409c8da9bbfe822ccd9d1dbd3ffeed3b3a37676c
超级厉害的萱萱 <luoxuan@MacBook-Pro-2.local>
2026-04-13 12:10:27 +0800

    first commit

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ REPO_NAME = 'super-resolution-and-RL'
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["image super-resolution", "super-resolution", "SR", "super resolution", "super-resolution reconstruction", "super-resolution restoration", "super-resolution enhancement", "super-resolution generation", "super-resolution prediction", "super-resolution synthesis", "super-resolution transformation", "super-resolution upscaling", "super-resolution upsampling","reinforcement learning", "RL", "deep reinforcement learning", "DRL", "reinforcement learning-based"]
-
+KEYWORD_LIST = ["image super-resolution"]
 OPENAI_API_KEYS = ["sk-nG2b2a92a6ad1c499d24d5c93436a6d5a61ce779e1885ZQ9"]
 LANGUAGE = "zh" 
+
